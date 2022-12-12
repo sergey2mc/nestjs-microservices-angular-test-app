d93c534186295664d6d060086f3c971824ce71fc
Serhii Yermakov <sergey@2muchcoffee.com>
2022-12-12 15:14:15 +0200

    feat (server:apps:api): add cors option

--- server/apps/api/src/main.ts
+++ server/apps/api/src/main.ts
@@ -6,7 +6,9 @@ import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
